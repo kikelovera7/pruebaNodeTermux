01ef7d52a97164ac0de1a777e15f5bbe8af3d1b1
Enrique Lovera <44738179+kikelovera7@users.noreply.github.com>
2022-10-13 09:54:37 -0300

    Adding simple communication between

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("HOLAAAA MUNDO")
+var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+var DeviceClient = require('azure-iot-device').Client;
+var Message = require('azure-iot-device').Message;
+
+var connectionString = 'HostName=asa-EnriqueNodeJSTEST.azure-devices.net;DeviceId=DevicePrueba123;SharedAccessKey=GGH/mR396SROTfV1IVQQymxKPHi9Jy0mSJFN569uFlI=';
+
+
+///RECEIVING MESSAGES
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+// LISTENING NEW MESSAGES SUBSCRIBING
+client.on('message',function(msg){
+    console.log("MESSAGE : ", msg);
+    console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
+    console.log(msg.properties);
+    client.complete(msg)
+    .then(data=>{
+        console.log("DATA : ",data);
+    })
+    .catch(error=>{
+        console.log("ERROR :", error)
+    })
+    ;
+});
+
